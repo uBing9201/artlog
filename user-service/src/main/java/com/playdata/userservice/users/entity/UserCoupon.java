6d67d1a575d630f4adc991c00fc3e640d811c112
ubing <ubing0101@kakao.com>
2025-05-09 11:23:27 +0900

    userCoupon 엔티티 생성

---
+++ user-service/src/main/java/com/playdata/userservice/users/entity/UserCoupon.java
@@ -0,0 +1,48 @@
+package com.playdata.userservice.users.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "UserCoupons")
+public class UserCoupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_key")
+    private User user;
+
+    @Column(name = "coupon_key")
+    private Long couponKey;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 1)
+    private YnType active;
+
+    @Embedded
+    private BaseEntity baseEntity;
+
+}
