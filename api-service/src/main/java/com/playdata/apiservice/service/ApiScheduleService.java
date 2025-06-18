a09c8f15a3c2320942a5bfb8824a19e92de13d62
Devy17 <devyks107@gmail.com>
2025-06-18 15:24:46 +0900

    cronjob 추가

---
+++ api-service/src/main/java/com/playdata/apiservice/service/ApiScheduleService.java
@@ -0,0 +1,28 @@
+package com.playdata.apiservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ApiScheduleService {
+    private final ApiCacheService apiCacheService;
+    private final ApiDataService apiDataService;
+
+    @Scheduled(cron = "0 0 3 * * *")
+    public void checkApiSchedule(){
+        try {
+            apiCacheService.deleteOrderCache();
+            apiDataService.getApiData(1);
+            log.info("최신 정보를 불러오는 데 성공하였습니다.");
+        } catch (IOException e) {
+            log.info("최신 정보를 불러오는 데 실패하였습니다.");
+            return;
+        }
+    }
+}
